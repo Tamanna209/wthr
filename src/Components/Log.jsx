7143c66bf09affd89320ddfec8a6d7fb3726431b
Tamanna209 <metamannaw3@gmail.com>
2024-11-08 16:04:35 +0530

    after update

--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "../Styles/LogStyle.css";
-
+import Logo from "../assets/logo.png";
   const api={
     baseUrl:"https://api.openweathermap.org/data/2.5/", 
     apiKey:"6412aa5d72ffbd58b1de8c69c83553f1"
@@ -29,7 +29,8 @@ const Log = () => {
   return (
     <div className='Main'>
         <div>
-            <h1>Check Current Weather </h1>
+            <h1><center>Check Current Weather</center> </h1>
+            <img src={Logo} alt="" className='image'/>
             <div className='data'>
                 <input type="text" 
                 placeholder='Enter City Name' 
@@ -42,10 +43,10 @@ const Log = () => {
             { ( typeof result.sys != "undefined")? (
                 <div className='result'>
                    <div>
-                   {result.name} , {result.sys.country}
+                 <span style={{fontSize:"25px" , fontWeight:"bolder", color:'darkblue'}}>City :</span>  {result.name} , {result.sys.country}
                </div>
-               <div>{result.main.temp} Temprature</div>
-                <div>{result.weather[0].description}</div>
+               <div><span style={{fontSize:"25px" , fontWeight:"bolder", color:'darkblue'}}>Temprature :</span>{result.main.temp}°C</div>
+                <div> <span style={{fontSize:"25px" , fontWeight:"bolder", color:'darkblue'}}>Weather :</span>{result.weather[0].description}</div>
                </div>
                 
             ):(" ") }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "../Styles/NavStyle.css";
+
 const Navbar = () => {
   return (
     <div>
         <div className='navbar'>
-           <div className='div1'><p>WEATHER APP</p> </div> 
+           <div className='div1'>
+           
+            <p><span style={{color:"orangered"}}>WEATHER</span><span>APP</span></p> </div> 
            <div className='div2'>
             <p>{new Date().toLocaleDateString()}</p>
-           
             </div>
             </div>
     </div>
